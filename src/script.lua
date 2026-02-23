9728130b78c1c528e428b2a941369d1a3f42b5a5
chore_3 <anon408chore@gmail.com>
2026-02-23 23:32:37 +0900

    test: utilの動作確認用プログラムを追加

--- src/script.lua
+++ src/script.lua
@@ -3,10 +3,13 @@ vanilla_model.ARMOR:setVisible(false)
 
 local safeAnim = require("modules/safeAnim")
 local stateHandler = require("modules/stateHandler")
+local util = require("modules/util")
 
 -- ==================================================
 
 
 function events.tick()
-    log(stateHandler.isIdle())
+    safeAnim.setPlayIfExists(animations.model, "unexpected", true)
+    safeAnim.setPlayIfExists(animations.skull, "sample", true)
+    log(util.getUnexpectedAnimations(animations.model, stateHandler.states))
 end
