00c42f49f52d9b8cfbab0544b81d0de7ec1fbf03
chore_3 <anon408chore@gmail.com>
2026-02-23 23:33:29 +0900

    add: script.luaにstateHandlerのハンドラーを作成

--- src/script.lua
+++ src/script.lua
@@ -7,6 +7,71 @@ local util = require("modules/util")
 
 -- ==================================================
 
+stateHandler.onIdle(function()
+
+end)
+
+stateHandler.onWalk(function()
+
+end)
+
+stateHandler.onSprint(function()
+
+end)
+
+stateHandler.onCrouch(function()
+
+end)
+
+stateHandler.onJump(function()
+
+end)
+
+stateHandler.onSwim(function()
+
+end)
+
+stateHandler.onGlide(function()
+
+end)
+
+stateHandler.onClimb(function()
+
+end)
+
+stateHandler.onFall(function()
+
+end)
+
+stateHandler.onBlock(function()
+
+end)
+
+stateHandler.onChat(function()
+
+end)
+
+stateHandler.onInventory(function()
+
+end)
+
+stateHandler.onFishing(function()
+
+end)
+
+stateHandler.onRiptide(function()
+
+end)
+
+stateHandler.onSleep(function()
+
+end)
+
+stateHandler.onDye(function()
+
+end)
+
+-- ==================================================
 
 function events.tick()
     safeAnim.setPlayIfExists(animations.model, "unexpected", true)
