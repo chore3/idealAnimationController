c22e48727ab1b69264dcec1bf3b60cc29a03c700
chore_3 <anon408chore@gmail.com>
2026-02-24 11:12:48 +0900

    add: プログラムの説明を追加

--- src/onStateEvent.lua
+++ src/onStateEvent.lua
@@ -1,3 +1,7 @@
+-- # ステートイベント
+-- [stateHandler.statesの値がtrueになった瞬間のみ実行される関数です。]
+-- [script.lua内の`require("src/onStateEvent")`のコメントアウト(-- )を削除することで動作を確認できます。]
+
 stateHandler.onIdle(function()
     log("idle")
 end)
